47ee23d92686ffd3c929e4cef05915bc03e067e1
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-04-06 16:56:40 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -9,17 +9,17 @@ void main(){
   double subtracao = valor1 - valor2;
   double multiplicacao = valor1 * valor2;
   double divisao = valor1 / valor2;
-  double restodadivisao = valor1 % valor2;
-  var potencia = pow(valor1,valor2);
-  double raiz = sqrt(valor1);
+  double restoDaDivisao = valor1 % valor2;
+  double potencia = pow(valor1,valor2) as double;
+  double raiz = pow(valor1, 1 / valor2) as double;
   
 
   print("A soma entre $valor1 e $valor2 é $soma");
   print("A subtracao entre $valor1 e $valor2 é $subtracao");
   print("A multiplicacao entre $valor1 e $valor2 é $multiplicacao");
   print("A divisao entre $valor1 e $valor2 é $divisao");
-  print("A restodadivisao entre $valor1 e $valor2 é $restodadivisao");
+  print("A restoDaDivisao entre $valor1 e $valor2 é $restoDaDivisao");
   print("A potencia entre $valor1 e $valor2 é $potencia");
-  print("A raiz quadrada de $valor1 é $raiz");
+  print("A raiz quadrada entre $valor1 e $valor2 é $raiz");
 
 }
