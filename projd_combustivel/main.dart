1129f8cab6a9f66eb4d11eb6f958ced03ee715b2
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-04-15 16:29:10 -0300

    Add files via upload

---
+++ projd_combustivel/main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main(){
+
+stdout.write("\nDigite o preço do litro de combustível:");
+double litro = double.parse(stdin.readLineSync()!);
+
+stdout.write("\nDigite o consumo em Litros do veiculo:");
+double Kml = double.parse(stdin.readLineSync()!);
+
+stdout.write("\nDigite a distancia a percorrer:");
+double Km = double.parse(stdin.readLineSync()!);
+
+double consumo = Km * Kml;
+
+double valor = consumo / litro;
+
+print("\nSerão necessarios $consumo para realizar esta viagem");
+print("Você terá um gasto de $valor");
+
+
+}
