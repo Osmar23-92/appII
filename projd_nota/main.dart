04d6b5b3f0f2ae55eac5d0930f2cf74a5f7c3d80
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-04-15 15:42:19 -0300

    Add files via upload

--- projd_nota/main.dart
+++ projd_nota/main.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 
 void main(){
 
-  stdout.write("\nNome do aluno:\n");
+  stdout.write("\nNome do aluno:");
   String Nome = stdin.readLineSync()!;
 
-  stdout.write("\nDigite a primeira nota:\n");
+  stdout.write("\nDigite a primeira nota:");
   double Nota1 = double.parse(stdin.readLineSync()!);
 
-  stdout.write("\nDigite a segunda nota:\n");
+  stdout.write("\nDigite a segunda nota:");
   double Nota2 = double.parse(stdin.readLineSync()!);
 
   double Media = Nota1 + Nota2 / 2;
@@ -17,7 +17,7 @@ void main(){
   print("\nNome: $Nome");
   print("\nPrimeira nota: $Nota1");
   print("\nSegunda nota: $Nota2");
-  print("\nSua média de nota é $Media");
+  print("\nSua média de nota é :$Media");
   
 
 
