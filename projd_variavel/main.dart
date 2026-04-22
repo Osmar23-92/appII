1fac77bfa2f88c61fd1f41114ab22db9b22fbb86
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-04-22 15:00:36 -0300

    Add files via upload

--- projd_variavel/main.dart
+++ projd_variavel/main.dart
@@ -1,6 +1,7 @@
- import 'dart:io';
- void main (){
- 
+import 'dart:io';
+
+void main(){
+
 stdout.write("\nDigite o primeiro valor inteiro: ");
 int valor1 = int.parse(stdin.readLineSync()!);
 
@@ -10,12 +11,11 @@ int valor2 = int.parse(stdin.readLineSync()!);
 stdout.write("\nDigite o terceiro valor inteiro: ");
 int valor3 = int.parse(stdin.readLineSync()!);
 
-int temp = valor1;
+int aux = valor1;
 
 valor1 = valor2;
 valor2 = valor3;
-valor3 = temp;
-
+valor3 = valor1;
 
 print("primeiro valor digitado $valor1");
 print("segundo valor digitado $valor2");
