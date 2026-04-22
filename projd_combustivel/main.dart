870eac34755c973c00ef65773978379dc6e5f1fe
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-04-22 15:00:58 -0300

    Add files via upload

--- projd_combustivel/main.dart
+++ projd_combustivel/main.dart
@@ -1,22 +1,25 @@
 import 'dart:io';
 
 void main(){
+  stdout.write("\nInforme os dados para a viagem\n"
+  "Preço do combustível: ");
+  double precoLitro = double.parse(stdin.readLineSync()!);
 
-stdout.write("\nDigite o preço do litro de combustível:");
-double litro = double.parse(stdin.readLineSync()!);
+  stdout.write("Autonomia: ");
+  double autonomia = double.parse(stdin.readLineSync()!);
 
-stdout.write("\nDigite o consumo em Litros do veiculo:");
-double Kml = double.parse(stdin.readLineSync()!);
+  stdout.write("Distancia a percorrer/percorrida: ");
+  double distancia = double.parse(stdin.readLineSync()!);
 
-stdout.write("\nDigite a distancia a percorrer:");
-double Km = double.parse(stdin.readLineSync()!);
+  
+  double qtdLitros = distancia / autonomia;
+  double valorTotalDaViagem = qtdLitros * precoLitro;
 
-double consumo = Km * Kml;
-
-double valor = consumo / litro;
-
-print("\nSerão necessarios $consumo para realizar esta viagem");
-print("Você terá um gasto de $valor");
-
+  print("\nDados da viagem\n"
+  "Preço do combustível (litro): $precoLitro\n"
+  "Autonomia do veículo: $autonomia\n"
+  "Distancia a percorrer/percorrida: $distancia\n"
+  "Quantidade de litros gastos: $qtdLitros litros\n"
+  "Valor total gasto com a viagem: R\$${valorTotalDaViagem.toStringAsFixed(2)}\n");
 
 }
