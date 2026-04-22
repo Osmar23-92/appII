8cd43629d8405051067b554cfa120d7957c67991
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-04-22 16:58:38 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,23 +1,14 @@
+import 'package:flutter/material.dart';
 
-import 'dart:io';
+void main() {
+runApp(MeuApp());
+}
 
-void main (){
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
 
-stdout.write("Nome do funcionário: ");
-String? nome = stdin.readLineSync();
-
-stdout.write("Valor da hora: ");
-double Valorhora = double.parse(stdin.readLineSync()!);
-
-stdout.write("Carga horária: ");
-double Cargahora = double.parse(stdin.readLineSync()!);
-
-double multiplicacao = Valorhora * Cargahora;
-
-
-print("Olá, $nome");
-print("O valor da sua hora $Valorhora");
-print("Sua carga horária mensal é $Cargahora horas");
-print("Salário base mensal $multiplicacao reais");
-
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
 }
